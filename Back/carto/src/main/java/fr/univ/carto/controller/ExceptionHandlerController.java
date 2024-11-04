f4e5760ddd0e1f5551e12de3c59fc73e0eeace09
felix.vuylsteker.etu <felix.vuylsteker.etu@univ-lille.fr>
2024-11-04 12:58:38 +0100

    Change import

--- Back/carto/src/main/java/fr/univ/carto/controller/ExceptionHandlerController.java
+++ Back/carto/src/main/java/fr/univ/carto/controller/ExceptionHandlerController.java
@@ -3,7 +3,6 @@ package fr.univ.carto.controller;
 import fr.univ.carto.exception.EstablishmentNotFoundException;
 import fr.univ.carto.exception.InvalidEstablishmentException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
