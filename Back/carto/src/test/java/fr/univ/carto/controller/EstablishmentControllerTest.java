1a65f344c91117180e3f73f557eaa47b7dd6cbd4
felix.vuylsteker.etu <felix.vuylsteker.etu@univ-lille.fr>
2024-11-10 14:34:33 +0100

    getEstablishmentByIDTest

--- Back/carto/src/test/java/fr/univ/carto/controller/EstablishmentControllerTest.java
+++ Back/carto/src/test/java/fr/univ/carto/controller/EstablishmentControllerTest.java
@@ -1,12 +1,14 @@
 package fr.univ.carto.controller;
 
 import fr.univ.carto.controller.dto.EstablishmentDto;
+import fr.univ.carto.exception.EstablishmentNotFoundException;
 import fr.univ.carto.service.EstablishmentService;
 import fr.univ.carto.service.bo.EstablishmentBo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -53,5 +55,28 @@ class EstablishmentControllerTest {
         assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
     }
 
+    @Test
+    void getEstablishmentByIdWhenIdInBaseTest(){
+        try {
+            when(establishmentService.getEstablishmentById(1L)).thenReturn(establishmentBoInDB);
 
+            ResponseEntity<EstablishmentDto> responseEntity = this.establishmentController.getEstablishmentById(1L);
+            assertEquals(EstablishmentDto.fromBo(establishmentBoInDB),responseEntity.getBody());
+            assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
+        } catch (EstablishmentNotFoundException e){
+            fail();
+        }
+    }
+
+    @Test
+    void getEstablishmentByIdWhenIdNotInBaseTest() throws EstablishmentNotFoundException {
+            when(establishmentService.getEstablishmentById(2L)).thenThrow(new EstablishmentNotFoundException("No establishment found for given id"));
+            EstablishmentNotFoundException exception = assertThrows(
+                EstablishmentNotFoundException.class,
+                    ()->establishmentController.getEstablishmentById(2L)
+            );
+            assertEquals("No establishment found for given id",exception.getMessage());
+
+    }
 }
+
