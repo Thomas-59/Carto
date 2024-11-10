dbf0d73f41da51b03e51e54f8d5112e49afac919
felix.vuylsteker.etu <felix.vuylsteker.etu@univ-lille.fr>
2024-11-10 18:04:57 +0100

    End of test for controller

--- Back/carto/src/test/java/fr/univ/carto/controller/EstablishmentControllerTest.java
+++ Back/carto/src/test/java/fr/univ/carto/controller/EstablishmentControllerTest.java
@@ -2,22 +2,23 @@ package fr.univ.carto.controller;
 
 import fr.univ.carto.controller.dto.EstablishmentDto;
 import fr.univ.carto.exception.EstablishmentNotFoundException;
+import fr.univ.carto.exception.InvalidEstablishmentException;
 import fr.univ.carto.service.EstablishmentService;
 import fr.univ.carto.service.bo.EstablishmentBo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,6 +29,7 @@ class EstablishmentControllerTest {
     private EstablishmentBo establishmentBoInDB;
     private List<EstablishmentDto> establishmentDtoListInDB;
     private List<EstablishmentBo> establishmentBOList;
+    private EstablishmentDto invalidDto;
     @BeforeEach
     void init(){
         this.establishmentController = new EstablishmentController(establishmentService);
@@ -44,6 +46,14 @@ class EstablishmentControllerTest {
         this.establishmentDtoListInDB = establishmentBOList.stream()
                 .map(EstablishmentDto::fromBo)
                 .toList();
+        this.invalidDto = EstablishmentDto.builder()
+                .name("name")
+                .description("description")
+                .latitude(190.0)
+                .longitude(50.0)
+                .cityName("cityname")
+                .build();
+
     }
 
     @Test
@@ -77,6 +87,67 @@ class EstablishmentControllerTest {
             );
             assertEquals("No establishment found for given id",exception.getMessage());
 
+    }
+
+    @Test
+    void createEstablishmentWithValidEstablishmentTest() throws InvalidEstablishmentException {
+        when(establishmentService.createEstablishment(any())).thenReturn(1L);
+        ResponseEntity<Long> responseEntity =this.establishmentController.createEstablishment(EstablishmentDto.fromBo(this.establishmentBoInDB));
+        assertEquals(1L,responseEntity.getBody());
+        assertEquals(HttpStatus.CREATED,responseEntity.getStatusCode());
+    }
+
+    @Test
+    void createEstablishmentWithInvalidEstablishmentTest(){
+        InvalidEstablishmentException exception = assertThrows(
+                InvalidEstablishmentException.class,
+                ()->establishmentController.createEstablishment(invalidDto)
+        );
+        assertEquals("invalid establishment",exception.getMessage());
+    }
+
+    @Test
+    void updateEstablishmentWithValidEstablishmentTest() throws InvalidEstablishmentException,EstablishmentNotFoundException{
+        ResponseEntity<Object> responseEntity = this.establishmentController.updateEstablishment(EstablishmentDto.fromBo(establishmentBoInDB));
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Test
+    void updateEstablishmentWithInvalidEstablishmentTest(){
+        InvalidEstablishmentException exception = assertThrows(
+                InvalidEstablishmentException.class,
+                ()->establishmentController.updateEstablishment(invalidDto)
+        );
+        assertEquals("Invalid Establishment given",exception.getMessage());
+    }
+
+    @Test
+    void updateEstablishmentWithValidButNotFoundEstablishmentTest() throws EstablishmentNotFoundException {
+        establishmentBoInDB.setId(2L);
+        doThrow(new EstablishmentNotFoundException("No establishment found for given id"))
+                .when(establishmentService).update(any());
+        EstablishmentNotFoundException exception = assertThrows(
+                EstablishmentNotFoundException.class,
+                ()->establishmentController.updateEstablishment(EstablishmentDto.fromBo(establishmentBoInDB))
+        );
+        assertEquals("No establishment found for given id",exception.getMessage());
+    }
+
+    @Test
+    void deleteEstablishmentTest() throws EstablishmentNotFoundException {
+        ResponseEntity<Object> responseEntity = this.establishmentController.deleteEstablishment(1L);
+        assertEquals(HttpStatus.ACCEPTED,responseEntity.getStatusCode());
+    }
+
+    @Test
+    void deleteEstablishmentIdNotFound() throws EstablishmentNotFoundException {
+        doThrow(new EstablishmentNotFoundException("No establishment found for given id"))
+                .when(establishmentService).deleteEstablishment(any());
+        EstablishmentNotFoundException exception = assertThrows(
+                EstablishmentNotFoundException.class,
+                ()->establishmentController.deleteEstablishment(2L)
+        );
+        assertEquals("No establishment found for given id",exception.getMessage());
     }
 }
 
