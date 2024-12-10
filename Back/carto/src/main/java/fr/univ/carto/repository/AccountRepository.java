421c87063df645eef76d219ebbb14c22cf030577
LaurineLeulliette <laurine.leulliette.etu@univ-lille.fr>
2024-12-10 11:44:28 +0100

    <feat> : create account via post request on /account

---
+++ Back/carto/src/main/java/fr/univ/carto/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package fr.univ.carto.repository;
+
+import fr.univ.carto.repository.entity.AccountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+}
