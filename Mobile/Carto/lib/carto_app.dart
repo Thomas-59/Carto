2f85c2a1266df4ae6a3b82130a07aa57a25acb92
LaurineLeulliette <laurine.leulliette.etu@univ-lille.fr>
2024-12-27 11:50:39 +0100

    <delete> unused bloc

--- Mobile/Carto/lib/carto_app.dart
+++ Mobile/Carto/lib/carto_app.dart
@@ -1,5 +1,3 @@
-import 'package:carto/cubit/location_cubit.dart';
-import 'package:carto/views/pages/address_input_page.dart';
 import 'package:carto/views/pages/establishement_details_page.dart';
 import 'package:carto/views/pages/home_page.dart';
 import 'package:carto/views/pages/search_page.dart';
@@ -8,7 +6,6 @@ import 'package:carto/views/pages/suggestion_page.dart';
 import 'package:carto/views/pages/thanking_page.dart';
 import 'package:carto/views/services/location_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartoApp extends StatelessWidget {
   final LocationService locationService;
@@ -18,23 +15,20 @@ class CartoApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-        create: (_) => LocationCubit(locationService),
-        child: MaterialApp(
-          title: 'Carto',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          routes: {
-            '/': (context) => const MyHomePage(),
-            '/suggestion': (context) => const SuggestionPage(),
-            '/thank' : (context) => const ThankingPage(),
-            '/etablishment_detail' : (context) => const EstablishmentDisplayPage(),
-            '/signup' : (context) => const SignUpPage(),
-            '/search' : (context) => const SearchPage(),
-          }
-        ),
+    return MaterialApp(
+      title: 'Carto',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: {
+        '/': (context) => const MyHomePage(),
+        '/suggestion': (context) => const SuggestionPage(),
+        '/thank': (context) => const ThankingPage(),
+        '/etablishment_detail': (context) => const EstablishmentDisplayPage(),
+        '/signup': (context) => const SignUpPage(),
+        '/search': (context) => const SearchPage(),
+      },
     );
   }
 }
